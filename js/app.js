5753a56eca4eedcafecea0e2f5174c7d07befb7b
Adam Fairhead <adam@adamfairhead.com>
2014-03-15 11:09:06 -0500

    Orbit stack on small

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).foundation({
       timer_speed: 10000, // Sets the amount of time in milliseconds before transitioning a slide
       pause_on_hover: false, // Pauses on the current slide while hovering
       animation_speed: 500, // Sets the amount of time in milliseconds the transition between slides will last
-      stack_on_small: false,
+      stack_on_small: true,
       navigation_arrows: true,
       slide_number: false,
       bullets_active_class: 'active', // Class name given to the active bullet
